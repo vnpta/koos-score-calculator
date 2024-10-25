ea03676fb353e8d3b9aa85ad8fa19464f3c69213
minhhoang968 <hoangtran1483@gmail.com>
2024-10-26 01:39:07 +0700

    script.js

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
     let total = 0;
     let count = 0;
 
-    // Lặp qua các câu hỏi để tính điểm
+    // Mảng chứa tên các câu hỏi
     const questions = [
         'symptom1',
+        'symptom2',
+        'pain1',
         // Thêm các câu hỏi khác vào đây
     ];
 
     questions.forEach(question => {
-        const radios = document.getElementsByName(question);
-        for (const radio of radios) {
-            if (radio.checked) {
-                total += parseInt(radio.value);
-                count++;
-            }
-        }
+        const select = document.getElementsByName(question)[0];
+        total += parseInt(select.value);
+        count++;
     });
 
     const avg = (total / (count * 4)) * 100; // Tính điểm phần trăm
     document.getElementById('result').innerText = `Điểm KOOS của bạn là: ${avg.toFixed(2)}%`;
 });
+
