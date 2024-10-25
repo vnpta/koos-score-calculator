6a0e5765d4b27071700c549544a83a406c350ab1
minhhoang968 <hoangtran1483@gmail.com>
2024-10-26 04:08:38 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function () {
+        // Bỏ chọn các checkbox khác trong cùng nhóm
+        if (this.checked) {
+            checkboxes.forEach((other) => {
+                if (other !== this) {
+                    other.checked = false;
+                }
+            });
+        }
+    });
+});
+
 function calculateScore() {
     const form = document.getElementById('koos-form');
     const formData = new FormData(form);
